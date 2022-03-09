31b9554abf20d422d179a693c936c0a3db3055d7
Bobby Moynihan <bobby@gigfirm.com>
2022-03-09 05:02:53 -0600

    delete button

---
+++ components/ButtonDelete.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="button-delete flex items-center content-end transition-all ease-quick-in">
+    <gButton 
+      v-if="confirming"
+      class="bg-red-500 text-red-900 hover:bg-red-600 text-xs mr-2"
+      @click="$emit('delete', true)">
+      Confirm Delete
+    </gButton>
+    <gButton 
+      v-tooltip="{content: confirming ? 'Cancel' : 'Delete This', placement: confirming ? 'top-end' : 'left' }"
+      tabindex="-1"
+      class="icon-button rounded-full text-gray-500 bg-gray-200 dark:bg-gray-600 dark:text-gray-900 shadow-xl"
+      :class="{
+        'hover:text-red-900 dark:hover:text-red-900 dark:hover:bg-red-500 hover:bg-red-500': !confirming
+      }"
+      @click="confirming = !confirming">
+      <Icon :name="confirming ? 'close' : 'trashcan'" class="relative top-px" />
+    </gButton>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tabindex: {
+      type: String,
+      default: '-1'
+    }
+  },
+  data() {
+    return {
+      confirming: false
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.button-delete .icon-button {
+  @apply w-8 h-8 flex items-center content-center;
+}
+// .tooltip
+.tooltip-inner {
+  @apply bg-red-400 text-red-900;
+}
+</style>
